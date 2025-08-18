9109a6892230b28219295041bfa3a6802e182d2f
Lovish Garg <52481365+lovish01@users.noreply.github.com>
2025-08-18 13:00:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'Auto-Recon-Engine'  // <-- change this if your repo name is different
-const username = 'lovish01'      // <-- change this to your GitHub username
+// ⚡ change repoName to your actual repo name
+const repoName = 'Auto-Recon-Engine'  
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,   // Important for GitHub Pages
+  base: `/${repoName}/`,  // 👈 this is the key fix
   build: {
     outDir: 'dist'
   }
